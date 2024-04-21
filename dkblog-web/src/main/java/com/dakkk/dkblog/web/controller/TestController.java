b17c76b8413f4118c8c35d2766567e0b9e18454f
dakavin <mikeylay@126.com>
2024-04-21 20:51:31 +0800

    web中使用JSR对实体类进行参数校验

--- dkblog-web/src/main/java/com/dakkk/dkblog/web/controller/TestController.java
+++ dkblog-web/src/main/java/com/dakkk/dkblog/web/controller/TestController.java
@@ -3,9 +3,15 @@ package com.dakkk.dkblog.web.controller;
 import com.dakkk.dkblog.web.model.User;
 import com.dakkk.dkblog.common.aspect.ApiOperationLog;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 /**
  * ClassName: TestController
@@ -18,9 +24,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class TestController {
-    @PostMapping("/test")
+    @PostMapping("/test1")
     @ApiOperationLog(description = "测试日志切面的接口")
-    public User test(@RequestBody User user){
+    public User test1(@RequestBody User user){
         return user;
+    }
+
+    @PostMapping("/test2")
+    @ApiOperationLog(description = "测试参数校验的接口")
+    public ResponseEntity<String> test2(@RequestBody @Validated User user, BindingResult bindingResult){
+        // 如果参数存在错误，则返回错误的参数
+        if (bindingResult.hasErrors()){
+            // 获取校验错误的每一个字段
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream().map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(","));
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+        // 没有错误
+        return ResponseEntity.ok("参数没有问题");
     }
 }
--- dkblog-web/src/main/java/com/dakkk/dkblog/web/model/User.java
+++ dkblog-web/src/main/java/com/dakkk/dkblog/web/model/User.java
@@ -1,6 +1,9 @@
 package com.dakkk.dkblog.web.model;
 
 import lombok.Data;
+import lombok.Value;
+
+import javax.validation.constraints.*;
 
 /**
  * ClassName: User
@@ -12,6 +15,15 @@ import lombok.Data;
  */
 @Data
 public class User {
+    @NotBlank(message = "用户名不能为空")
     private String username;
+
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 10, message = "年龄要求>=10")
+    @Max(value = 100, message = "年龄要求<=100")
     private Integer age;
+
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确！")
+    private String email;
 }
