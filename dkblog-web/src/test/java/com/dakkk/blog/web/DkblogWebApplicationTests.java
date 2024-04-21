f47d1ab7cf72b7422e9d91b97dbd10c1e62fe244
dakavin <mikeylay@126.com>
2024-04-21 19:49:05 +0800

    web添加日志配置，并测试

--- dkblog-web/src/test/java/com/dakkk/blog/web/DkblogWebApplicationTests.java
+++ dkblog-web/src/test/java/com/dakkk/blog/web/DkblogWebApplicationTests.java
@@ -1,13 +1,21 @@
 package com.dakkk.blog.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class DkblogWebApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    public void testLog(){
+        log.info("这是一行 info 级别的日志");
+        log.warn("这是一行 warn 级别的日志");
+        log.error("这是一行 error 级别的日志");
 
+        // 占位符测试
+        String author = "dakkk";
+        log.info("这是一行带有占位符日志，作者:{}",author);
+    }
 }
