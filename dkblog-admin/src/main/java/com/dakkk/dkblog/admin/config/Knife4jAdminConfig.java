26fd49c5cd8feaa0c400229dc217b34dce305dc9
dakavin <mikeylay@126.com>
2024-04-21 22:05:32 +0800

    使用Knife4j实现前台和后台接口的统一管理，并对接口进行测试

---
+++ dkblog-admin/src/main/java/com/dakkk/dkblog/admin/config/Knife4jAdminConfig.java
@@ -0,0 +1,55 @@
+package com.dakkk.dkblog.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * ClassName: Knife4jConfig
+ * Package: com.dakkk.dkblog.admin.config
+ *
+ * @Create 2024/4/21 21:53
+ * @Author dakkk
+ * Description:
+ */
+@Configuration
+@EnableSwagger2WebMvc
+@Profile("dev")
+public class Knife4jAdminConfig {
+    @Bean("adminApi")
+    public Docket createApiDoc(){
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(buildApiInfo())
+                .groupName("Admin 后台接口")
+                .select().apis(RequestHandlerSelectors.basePackage("com.dakkk.dkblog.admin.controller"))
+                .paths(PathSelectors.any())
+                .build();
+        return docket;
+    }
+
+    /**
+     * 构建 API 信息
+     */
+    private ApiInfo buildApiInfo(){
+        return new ApiInfoBuilder()
+                // 标题
+                .title("DK blog 博客后台接口文档")
+                // 描述
+                .description("DK blog 是一款由 Spring Boot + Vue 3.2 + Vite 4.3 开发的前后端分离博客。")
+                // API 服务条款
+                . termsOfServiceUrl("https://gitee.com/Dakkk_mike")
+                // 联系人
+                . contact(new Contact("dakkk","https://gitee.com/Dakkk_mike","mikeylay@126.com"))
+                // 版本号
+                .version("0.0.1")
+                .build();
+    }
+}
--- dkblog-web/src/main/java/com/dakkk/dkblog/web/controller/TestController.java
+++ dkblog-web/src/main/java/com/dakkk/dkblog/web/controller/TestController.java
@@ -5,6 +5,8 @@ import com.dakkk.dkblog.common.enums.ResponseErrorCodeEnum;
 import com.dakkk.dkblog.common.utils.Response;
 import com.dakkk.dkblog.web.model.User;
 import com.dakkk.dkblog.common.aspect.ApiOperationLog;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -26,15 +28,18 @@ import java.util.stream.Collectors;
  */
 @RestController
 @Slf4j
+@Api(tags = "测试模块")
 public class TestController {
     @PostMapping("/test1")
     @ApiOperationLog(description = "测试日志切面的接口")
+    @ApiOperation("测试日志切面的接口")
     public User test1(@RequestBody User user){
         return user;
     }
 
     @PostMapping("/test2")
     @ApiOperationLog(description = "测试参数校验的接口")
+    @ApiOperation("测试参数校验的接口")
     public ResponseEntity<String> test2(@RequestBody @Validated User user, BindingResult bindingResult){
         // 如果参数存在错误，则返回错误的参数
         if (bindingResult.hasErrors()){
@@ -50,6 +55,7 @@ public class TestController {
 
     @PostMapping("/test3")
     @ApiOperationLog(description = "测试全局响应的接口")
+    @ApiOperation("测试全局响应的接口")
     public Response test3(@RequestBody @Validated User user, BindingResult bindingResult){
         // 如果参数存在错误，则返回错误的参数
         if (bindingResult.hasErrors()){
@@ -65,6 +71,7 @@ public class TestController {
 
     @PostMapping("/test4")
     @ApiOperationLog(description = "测试BizException异常响应的接口")
+    @ApiOperation("测试BizException异常响应的接口")
     public Response test4(@RequestBody @Validated User user, BindingResult bindingResult){
         // 手动抛异常，入参是前面定义好的异常码枚举，返回统一交给全局处理器搞定
         throw new BizException(ResponseErrorCodeEnum.PRODUCT_NOT_FOUND);
@@ -72,6 +79,7 @@ public class TestController {
 
     @PostMapping("/test5")
     @ApiOperationLog(description = "测试Exception异常响应的接口")
+    @ApiOperation("测试Exception异常响应的接口")
     public Response test5(@RequestBody @Validated User user, BindingResult bindingResult){
         // 手动抛异常，入参是前面定义好的异常码枚举，返回统一交给全局处理器搞定
         int i = 1/0;
@@ -80,6 +88,7 @@ public class TestController {
 
     @PostMapping("/test6")
     @ApiOperationLog(description = "测试参数异常响应的接口")
+    @ApiOperation("测试参数异常响应的接口")
     public Response test6(@RequestBody @Validated User user){
         // 入参的时候，填写不符合标准的参数来测试
         System.out.println(user);
