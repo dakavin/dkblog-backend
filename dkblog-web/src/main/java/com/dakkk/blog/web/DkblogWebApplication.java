3e9c919165358b4d7a8247e611c7c6fcab06aabf
dakavin <mikeylay@126.com>
2024-04-21 19:34:37 +0800

    多模块项目初始化

---
+++ dkblog-web/src/main/java/com/dakkk/blog/web/DkblogWebApplication.java
@@ -0,0 +1,13 @@
+package com.dakkk.blog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkblogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DkblogWebApplication.class, args);
+    }
+
+}
---
+++ dkblog-web/src/test/java/com/dakkk/blog/web/DkblogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dakkk.blog.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkblogWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
