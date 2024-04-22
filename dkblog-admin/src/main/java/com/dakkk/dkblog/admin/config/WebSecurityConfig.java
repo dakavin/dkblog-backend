362c3163545b2fb4f1d011aaad723c1c76790814
dakavin <mikeylay@126.com>
2024-04-22 19:09:26 +0800

    admin注入jwt依赖，导入jwt模块中的jwt自定义用户认证

--- dkblog-admin/src/main/java/com/dakkk/dkblog/admin/config/WebSecurityConfig.java
+++ dkblog-admin/src/main/java/com/dakkk/dkblog/admin/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.dakkk.dkblog.admin.config;
 
+import com.dakkk.dkblog.jwt.config.JwtAuthenticationSecurityConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import javax.annotation.Resource;
 
 /**
  * ClassName: WebSecurityConfig
@@ -16,16 +20,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Resource
+    private JwtAuthenticationSecurityConfig jwtAuthenticaitonSecurityConfig;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                // 认证所有以 /admin 为前缀的 URL 资源
+        // 禁用 csrf
+        http.csrf().disable()
+                // 禁用表单登录,使用 JWT 登录不需要表单登录
+                .formLogin().disable()
+                // 应用jwt模块中自定义的用户认证配置
+                .apply(jwtAuthenticaitonSecurityConfig)
+                .and()
+                // 后续是配置 HTTP 请求的认证规则
+                .authorizeHttpRequests()
+                // 认证所有以 /admin 为前缀的 URL资源
                 .mvcMatchers("/admin/**").authenticated()
                 // 其他都需要放行，无需认证
-                .anyRequest().permitAll().and()
-                // 使用表单登录
-                .formLogin().and()
-                // 使用 HTTP Basic 认证
-                .httpBasic();
+                .anyRequest().permitAll()
+                .and()
+                // 前后端分离，设置会话管理策略为无状态的,即无需创建会话
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
