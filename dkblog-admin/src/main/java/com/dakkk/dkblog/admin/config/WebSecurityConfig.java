225b343b732355e79e7eef2cb4fdbc19b2735db2
dakavin <mikeylay@126.com>
2024-04-23 19:59:50 +0800

    在Admin模块下修改SS的全局配置，增加token鉴权

--- dkblog-admin/src/main/java/com/dakkk/dkblog/admin/config/WebSecurityConfig.java
+++ dkblog-admin/src/main/java/com/dakkk/dkblog/admin/config/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.dakkk.dkblog.admin.config;
 
 import com.dakkk.dkblog.jwt.config.JwtAuthenticationSecurityConfig;
+import com.dakkk.dkblog.jwt.filter.TokenAuthenticationFilter;
+import com.dakkk.dkblog.jwt.handler.RestAccessDeniedHandler;
+import com.dakkk.dkblog.jwt.handler.RestAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.annotation.Resource;
 
@@ -22,6 +27,10 @@ import javax.annotation.Resource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Resource
     private JwtAuthenticationSecurityConfig jwtAuthenticaitonSecurityConfig;
+    @Resource
+    private RestAuthenticationEntryPoint authEntryPoint;
+    @Resource
+    private RestAccessDeniedHandler deniedHandler;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 禁用 csrf
@@ -38,7 +47,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 其他都需要放行，无需认证
                 .anyRequest().permitAll()
                 .and()
+                // 处理用户未登录访问受保护的资源的情况
+                .httpBasic().authenticationEntryPoint(authEntryPoint)
+                .and()
+                // 处理登录成功后访问受保护的资源，但是权限不够的情况
+                .exceptionHandling().accessDeniedHandler(deniedHandler)
+                .and()
                 // 前后端分离，设置会话管理策略为无状态的,即无需创建会话
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // 将 Token 校验过滤器添加到用户认证过滤器之前
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    /**
+     * token 校验过滤器
+     */
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter(){
+        return new TokenAuthenticationFilter();
     }
 }
