40fa692875cdbea17ba44a6249a23b36fc81ff66
dakavin <mikeylay@126.com>
2024-05-19 12:31:16 +0800

    fix(Admin-BlogSettings)：修复博客设置，只有管理员用户才能保存博客设置

--- dkblog-admin/src/main/java/com/dakkk/dkblog/admin/controller/AdminBlogSettingsController.java
+++ dkblog-admin/src/main/java/com/dakkk/dkblog/admin/controller/AdminBlogSettingsController.java
@@ -6,6 +6,7 @@ import com.dakkk.dkblog.common.aspect.ApiOperationLog;
 import com.dakkk.dkblog.common.utils.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,6 +33,7 @@ public class AdminBlogSettingsController {
     @PostMapping("/update")
     @ApiOperation("1-博客设置更新")
     @ApiOperationLog(description = "博客设置更新")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Response updateBlogSettings(@RequestBody @Validated UpdateBlogSettingsReqVO
                                        updateBlogSettingsReqVO){
         return adminBlogSettingsService.updateBlogSettings(updateBlogSettingsReqVO);
